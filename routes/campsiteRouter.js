4629e3c0dc8ae7ebf2309e1def7652d0d3a3c6b2
Slim <sazwerling@gmail.com>
2021-01-02 18:26:14 -0700

    first workshop

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -18,11 +18,37 @@ campsiteRouter.route('/')
 
 .put((req, res) => {
     res.statusCode = 403;
-    res.end('Put operation not suported on /campsites')
+    res.end('Put operation not suported on /campsitesId')
 })
 
 .delete((req, res) => {
     res.end('Deleting all campsites')
+});
+
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain')
+    next()
+})
+
+.get((req, res) => {
+    res.end(`Will send details of the campsite ${req.campsiteId} to you`)
+})
+
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.campsiteId}`)
+})
+
+.put((req, res) => {
+    res.write(`Updating the campsite: ${req.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.name} 
+    with description: ${req.body.description}`)
+})
+
+.delete((req, res) => {
+    res.end(`Deleting campsite: ${req.campsiteId}`)
 });
 
 module.exports = campsiteRouter
